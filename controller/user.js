c82eeef447f13b0d41cd89242391edf16fac3636
Swapnil Makwana <105115503+swapnil1303@users.noreply.github.com>
2024-05-14 12:40:39 +0530

    check user already exists using roll number only

--- controller/user.js
+++ controller/user.js
@@ -6,7 +6,7 @@ export const signup = async (req, res) => {
   try {
     const { name, department, username, email, password } = req.body;
 
-    let user = await User.findOne({ username,department,email });
+    let user = await User.findOne({ username });
 
     if (user)
       return res.status(201).json({

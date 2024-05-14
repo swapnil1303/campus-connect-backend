fdf3d2b0e9b43fb283b9867bfb30bade0412e7ad
Swapnil Makwana <105115503+swapnil1303@users.noreply.github.com>
2024-05-14 13:32:57 +0530

    Update server.js updated cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:3000','https://campus-connect-frontend-6tk4.onrender.com']
+  origin: ['http://localhost:3000','https://campus-connect-frontend-mdur.onrender.com']
 }));
 
 // Using routes

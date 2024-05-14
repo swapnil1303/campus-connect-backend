88726ef8cb4047e1b2fd761b9ec2eabdfbcc7898
Swapnil Makwana <105115503+swapnil1303@users.noreply.github.com>
2024-05-14 13:18:11 +0530

    Update server.js allowing deployed version as well using cors origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000','https://campus-connect-frontend-6tk4.onrender.com']
 }));
 
 // Using routes
